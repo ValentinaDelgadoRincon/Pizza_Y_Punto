dae920deab658a58ac34bcd9c025bfe2d0b320ba
CamilaFlorez12 <07camilaflorezsantos@gmail.com>
2025-09-19 09:14:43 -0500

    feat: :sparkles: creacion modelo de datos
    creacion modelo de dato
    
    By@CamilaFlorez

---
+++ etructuraDB/ingredientes.js
@@ -0,0 +1,52 @@
+[
+    {
+        "nombre": "Mozzarella",
+        "tipo": "Queso",
+        "stock": 15
+    },
+    {
+        "nombre": "Parmesano",
+        "tipo": "Queso",
+        "stock": 10
+    },
+    {
+        "nombre": "Salsa de tomate",
+        "tipo": "Salsa",
+        "stock": 20
+    },
+    {
+        "nombre": "Salsa BBQ",
+        "tipo": "Salsa",
+        "stock": 12
+    },
+    {
+        "nombre": "Pechuga de pollo",
+        "tipo": "Proteína",
+        "stock": 8
+    },
+    {
+        "nombre": "Aceitunas negras",
+        "tipo": "Topping",
+        "stock": 25
+    },
+    {
+        "nombre": "Champiñones",
+        "tipo": "Topping",
+        "stock": 18
+    },
+    {
+        "nombre": "Pimiento verde",
+        "tipo": "Topping",
+        "stock": 22
+    },
+    {
+        "nombre": "Anchoas",
+        "tipo": "Topping",
+        "stock": 5
+    },
+    {
+        "nombre": "Albahaca fresca",
+        "tipo": "Hierba",
+        "stock": 30
+    }
+]
---
+++ etructuraDB/pizzas.js
@@ -0,0 +1,127 @@
+[
+    {
+        "nombre": "Pizza Margarita",
+        "ingredientes": [
+            {
+                "nombre": "Mozzarella",
+                "tipo": "Queso",
+                "stock": 15
+            },
+            {
+                "nombre": "Salsa de tomate",
+                "tipo": "Salsa",
+                "stock": 20
+            },
+            {
+                "nombre": "Albahaca fresca",
+                "tipo": "Hierba",
+                "stock": 30
+            }
+        ]
+    },
+    {
+        "nombre": "Pizza BBQ Pollo",
+        "ingredientes": [
+            {
+                "nombre": "Mozzarella",
+                "tipo": "Queso",
+                "stock": 15
+            },
+            {
+                "nombre": "Salsa BBQ",
+                "tipo": "Salsa",
+                "stock": 12
+            },
+            {
+                "nombre": "Pechuga de pollo",
+                "tipo": "Proteína",
+                "stock": 8
+            },
+            {
+                "nombre": "Pimiento verde",
+                "tipo": "Topping",
+                "stock": 22
+            }
+        ]
+    },
+    {
+        "nombre": "Pizza Hawaiana",
+        "ingredientes": [
+            {
+                "nombre": "Mozzarella",
+                "tipo": "Queso",
+                "stock": 15
+            },
+            {
+                "nombre": "Salsa de tomate",
+                "tipo": "Salsa",
+                "stock": 20
+            },
+            {
+                "nombre": "Pechuga de pollo",
+                "tipo": "Proteína",
+                "stock": 8
+            },
+            {
+                "nombre": "Aceitunas negras",
+                "tipo": "Topping",
+                "stock": 25
+            }
+        ]
+    },
+    {
+        "nombre": "Pizza Vegetariana",
+        "ingredientes": [
+            {
+                "nombre": "Mozzarella",
+                "tipo": "Queso",
+                "stock": 15
+            },
+            {
+                "nombre": "Salsa de tomate",
+                "tipo": "Salsa",
+                "stock": 20
+            },
+            {
+                "nombre": "Champiñones",
+                "tipo": "Topping",
+                "stock": 18
+            },
+            {
+                "nombre": "Pimiento verde",
+                "tipo": "Topping",
+                "stock": 22
+            },
+            {
+                "nombre": "Albahaca fresca",
+                "tipo": "Hierba",
+                "stock": 30
+            }
+        ]
+    },
+    {
+        "nombre": "Pizza Anchoas",
+        "ingredientes": [
+            {
+                "nombre": "Mozzarella",
+                "tipo": "Queso",
+                "stock": 15
+            },
+            {
+                "nombre": "Salsa de tomate",
+                "tipo": "Salsa",
+                "stock": 20
+            },
+            {
+                "nombre": "Anchoas",
+                "tipo": "Topping",
+                "stock": 5
+            },
+            {
+                "nombre": "Aceitunas negras",
+                "tipo": "Topping",
+                "stock": 25
+            }
+        ]
+    }
+]
