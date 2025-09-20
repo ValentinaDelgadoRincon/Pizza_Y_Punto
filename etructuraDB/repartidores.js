bae411cc5e488a91754e046390d9481f66ec167a
CamilaFlorez12 <07camilaflorezsantos@gmail.com>
2025-09-20 09:15:52 -0500

    docs: :memo: fix sitaxis
    fix sintaxis
    
    By@CamilaFlorez

--- etructuraDB/repartidores.js
+++ etructuraDB/repartidores.js
@@ -1,32 +1,32 @@
 [
     {
         _id:ObjectId('68cd7cecf4ed302639e4ce81'),
-        "nombre": "Dylan",
-        "zona": "Metropolitana",
-        "estado": "disponible"
+        nombre: "Dylan",
+        zona: "Metropolitana",
+        estado: "disponible"
     },
     {
         _id:ObjectId('68cd7cecf4ed302639e4ce82'),
-        "nombre": "Jefferson",
-        "zona": "Metropolitana",
-        "estado": "ocupado"
+        nombre: "Jefferson",
+        zona: "Metropolitana",
+        estado: "ocupado"
     },
     {
         _id:ObjectId('68cd7cecf4ed302639e4ce83'),
-        "nombre": "Sergio",
-        "zona": "Metropolitana",
-        "estado": "disponible"
+        nombre: "Sergio",
+        zona: "Metropolitana",
+        estado: "disponible"
     },
     {
         _id:ObjectId('68cd7cecf4ed302639e4ce84'),
-        "nombre": "Davisson",
-        "zona": "Metropolitana",
-        "estado": "ocupado"
+        nombre: "Davisson",
+        zona: "Metropolitana",
+        estado: "ocupado"
     },
     {
         _id:ObjectId('68cd7cecf4ed302639e4ce85'),
-        "nombre": "Juan",
-        "zona": "Metropolitana",
-        "estado": "disponible"
+        nombre: "Juan",
+        zona: "Metropolitana",
+        estado: "disponible"
     }
 ]
